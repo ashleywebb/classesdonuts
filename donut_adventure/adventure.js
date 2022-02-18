c5333975fce466b271fef90b4e918ead3ba2dadb
ashleywebb <ashleywebb112@gmail.com>
2022-02-17 23:34:23 -0700

    created our enemy pizza rat

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -19,3 +19,28 @@ class Hero{
         return "i\'m ready to rumble"
     }
 } 
+
+class Enemy {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.catchPhrases = ['i\'m youtube famous',
+        'i\'m more dangerous than an uncovered sewer']
+    }
+    weapons = {
+        pepperoniStars: 5,
+        cheeseGrease: 10
+    } 
+    talkSmack() {
+        return this.catchPhrases[Math.floor(math.random() * this.catchPhrases.length)]
+    }
+    announceHealth() {
+        return this.health
+    }
+    fight() {
+        return 'i\'m gonna flatten you like a slice of pepperoni!'
+    }
+}
+
+const hero = new Hero('Dougie')
+const enemy = new Enemy('Pizza Rat')
