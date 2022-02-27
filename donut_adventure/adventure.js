9392d5d31a7c6c846c65b4fd56ecc767476a9452
ashleywebb <ashleywebb112@gmail.com>
2022-02-27 02:01:32 -0700

    lab finished

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -10,13 +10,14 @@ class Hero{
         sugarShock: 10
     }
     talkSass() {
-        return this.catchPhrases[Math.floor(math.random() * this.catchPhrases.length)]
+        return this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]
     }
     announceHealth() {
         return this.health
     }
     fight() {
-        return "i\'m ready to rumble"
+        let weapons1 = Object.keys(this.weapons)
+        return weapons1[Math.floor(Math.random() * weapons1.length)]
     }
 } 
 
@@ -32,15 +33,25 @@ class Enemy {
         cheeseGrease: 10
     } 
     talkSmack() {
-        return this.catchPhrases[Math.floor(math.random() * this.catchPhrases.length)]
+        return this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]
     }
     announceHealth() {
         return this.health
     }
     fight() {
-        return 'i\'m gonna flatten you like a slice of pepperoni!'
+        let weapons2 = Object.keys(this.weapons)
+        return weapons2[Math.floor(Math.random() * weapons2.length)]
     }
 }
 
 const hero = new Hero('Dougie')
 const enemy = new Enemy('Pizza Rat')
+
+console.log(hero.talkSass())
+console.log(enemy.talkSmack())
+console.log(hero.announceHealth())
+console.log(enemy.announceHealth())
+console.log(hero.health -= enemy.fight())
+console.log(enemy.health -= hero.fight())
+console.log(enemy.announceHealth())
+console.log(hero.announceHealth())
